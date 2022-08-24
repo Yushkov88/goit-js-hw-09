c07ab18bdc18f782a7742069e6877c367333c131
Yushkov88 <yushkov1988@gmail.com>
2022-08-24 16:17:45 +0300

    hw9 task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  return promise;
 }
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const { delay, step, amount } = event.target.elements;
+
+  for (let i = 0; i < +amount.value; ++i) {
+    createPromise(i + 1, +delay.value + step.value * i)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
